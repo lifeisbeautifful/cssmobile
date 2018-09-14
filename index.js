b9d881cccb4699a1d80b4fc83302e45887984903
Оксана <oksankagnyp@gmail.com>
2018-09-14 14:15:06 +0000

    лв

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-;(function gallery(){
-    let leftArrow=document.getElementById('leftArrow');
-    let rightArrow=document.getElementById('rightArrow');
-    centerValue=1;
-    maxImageNumber=8;
+//;(function gallery(){
+ //   let leftArrow=document.getElementById('leftArrow');
+   // let rightArrow=document.getElementById('rightArrow');
+    //centerValue=1;
+//    maxImageNumber=8;
         
-        leftArrow.addEventListener('click', moveBackward);
-        rightArrow.addEventListener('click', moveForward);
+  //      leftArrow.addEventListener('click', moveBackward);
+    //    rightArrow.addEventListener('click', moveForward);
+      //  
+        //function moveBackward(){
+          //  let image=document.getElementById('image');
+            //centerValue--;
+    //        if(centerValue==0){centerValue=maxImageNumber};
+      //      image.setAttribute('src', ''+ centerValue+'.jpg');
+        //};
         
-        function moveBackward(){
-            let image=document.getElementById('image');
-            centerValue--;
-            if(centerValue==0){centerValue=maxImageNumber};
-            image.setAttribute('src', ''+ centerValue+'.jpg');
-        };
-        
-        function moveForward(){
-           let image=document.getElementById('image');
-            centerValue++;
-           if(centerValue> maxImageNumber){centerValue=1}
-           image.setAttribute('src', ''+ centerValue+'.jpg');
-        };
+    //    function moveForward(){
+      //     let image=document.getElementById('image');
+        //    centerValue++;
+          // if(centerValue> maxImageNumber){centerValue=1}
+        //   image.setAttribute('src', ''+ centerValue+'.jpg');
+    //    };
 
 
     
-})();
+//})();
